9391b4ebc65d2039049d35c550e1e3e18f6c6f33
Daniel Finneran <dan@Daniels-MacBook-Pro.local>
2015-05-20 18:28:38 +0100

    Initial Commit

---
+++ OVCLI/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  OVCLI
+//
+//  Created by Daniel Finneran on 20/05/2015.
+//  Copyright (c) 2015 Daniel Finneran. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
